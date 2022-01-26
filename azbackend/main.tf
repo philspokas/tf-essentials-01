ec3f30178c35e9d23a46e6a6a4d541fca4174d3e
philspokas <phil@intellitect.com>
2022-01-25 16:53:44 -0800

    add backend sample

--- azbackend/main.tf
+++ azbackend/main.tf
@@ -8,6 +8,13 @@ terraform {
         }
         random = "=3.1.0"
     }
+
+    backend "azurerm" {
+      resource_group_name  = "tf-essentials-01d"
+      storage_account_name = "tfessential01"
+      container_name       = "tfstate"
+      key                  = "prod.terraform.tfstate"  
+    }
 }
 
 provider "azurerm" {
@@ -15,30 +22,30 @@ provider "azurerm" {
 #  subscription_id = "e58fc1ee-ddc9-418f-9d25-0dd1879357ff"
 }
 
-resource "azurerm_resource_group" "tf_essential_01" {
+resource "azurerm_resource_group" "tf_essentials" {
   name     = var.resourcegroup_name
   location = "westus2"
 }
 
 resource "azurerm_storage_account" "tf_essential_sa" {
-  name                     = "tfessential01"
-  resource_group_name      = azurerm_resource_group.tf_essential_01.name
-  location                 = azurerm_resource_group.tf_essential_01.location
+  name                     = "tfessential02"
+  resource_group_name      = azurerm_resource_group.tf_essentials.name
+  location                 = azurerm_resource_group.tf_essentials.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "tf_essential_container" {
-  name                  = "tfstate"
+  name                  = "content"
   storage_account_name  = azurerm_storage_account.tf_essential_sa.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "the_blob" {
-  name                   = "terraform-this.jpg"
+  name                   = "terraform-that.jpg"
   storage_account_name   = azurerm_storage_account.tf_essential_sa.name
   storage_container_name = azurerm_storage_container.tf_essential_container.name
   type                   = "Block"
-  source                 = "C:\\temp\\terraform_this.jpg"
+  source                 = "C:\\temp\\terraform_that.jpg"
 }
 
