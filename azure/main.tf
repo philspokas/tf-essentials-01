92530a9f68727df0882bb025407f8ce937d27694
philspokas <phil@intellitect.com>
2022-01-25 16:04:52 -0800

    add storage

--- azure/main.tf
+++ azure/main.tf
@@ -15,8 +15,30 @@ provider "azurerm" {
 #  subscription_id = "e58fc1ee-ddc9-418f-9d25-0dd1879357ff"
 }
 
-resource "azurerm_resource_group" "tf-essential-01" {
+resource "azurerm_resource_group" "tf_essential_01" {
   name     = var.resourcegroup_name
   location = "westus2"
+}
+
+resource "azurerm_storage_account" "tf_essential_sa" {
+  name                     = "tfessential01"
+  resource_group_name      = azurerm_resource_group.tf_essential_01.name
+  location                 = azurerm_resource_group.tf_essential_01.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tf_essential_container" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.tf_essential_sa.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "the_blob" {
+  name                   = "terraform-this"
+  storage_account_name   = azurerm_storage_account.tf_essential_sa.name
+  storage_container_name = azurerm_storage_container.tf_essential_container.name
+  type                   = "Block"
+  source                 = "C:\temp\terraform_this.jpg"
 }
 
